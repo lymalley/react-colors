c84c95998620052e1bb300cc14891c7784999219
Lauren Malley <laurenymalley@gmail.com>
2018-07-12 15:12:04 -0400

    2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Colors from './pages/colors'
-import ShowColor from './pages/colors/show'
-import StarWars from './pages/starwars'
-import ShowStarWarsName from './pages/starwars/show'
-import BuzzWords from './pages/buzzwords'
-import ShowBuzzWord from './pages/buzzwords/show'
-import Cookies from './pages/cookies'
-import ShowCookie from './pages/cookies/show'
-import TestWithRouterPage from './pages/test-withrouter-page';
-
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Colors from "./pages/colors";
+import ShowColor from "./pages/colors/show";
+import StarWars from "./pages/starwars";
+import ShowStarWarsName from "./pages/starwars/show";
+import BuzzWords from "./pages/buzzwords";
+import ShowBuzzWord from "./pages/buzzwords/show";
+import Cookies from "./pages/cookies";
+import ShowCookie from "./pages/cookies/show";
+import TestWithRouterPage from "./pages/test-withrouter-page";
+import Menu from "./pages/menu";
 
 const App = props => {
   return (
     <BrowserRouter>
       <div>
         <Switch>
+          <Route exact path="/" component={Menu} />
           <Route exact path="/colors" component={Colors} />
           <Route exact path="/colors/:id" component={ShowColor} />
           <Route exact path="/starwars" component={StarWars} />
@@ -24,12 +25,15 @@ const App = props => {
           <Route exact path="/buzzwords/:id" component={ShowBuzzWord} />
           <Route exact path="/cookies" component={Cookies} />
           <Route exact path="/cookies/:id" component={ShowCookie} />
-          <Route exact path="/testwithrouterpage" component={TestWithRouterPage} />
+          <Route
+            exact
+            path="/testwithrouterpage"
+            component={TestWithRouterPage}
+          />
         </Switch>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,24 +1,26 @@
-/*  
-  TODO: 
-  
-  When the application starts display a menu page that contains an unordered list if items: 
-  - Colors
-  - Buzz Words
-  - Star Wars Names
-  - Fortune Cookie
-
-  When a menu item is clicked send the user to the appropriate "list" page. 
-
-  For example, clicking the "Colors" list item should 
-   send the user to the page that displays a list of colors
-*/
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = props => {
-    return (
-      <div>
-        <h1>Five in One</h1>
-        
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Five in One</h1>
+      <ul>
+        <li>
+          <Link to="/colors">Colors</Link>
+        </li>
+        <li>
+          <Link to="/buzzwords">Buzz Words</Link>
+        </li>
+        <li>
+          <Link to="/starwars">Star Wars Names</Link>
+        </li>
+        <li>
+          <Link to="/cookie">Fotune Cookie</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
